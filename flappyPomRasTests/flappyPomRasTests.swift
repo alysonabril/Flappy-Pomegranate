67fb6723d248ea485b80813ed9cc22bb3fa28c99
alysonabril <alysonabril@pursuit.org>
2019-09-13 15:22:24 -0400

    initial commit

---
+++ flappyPomRasTests/flappyPomRasTests.swift
@@ -0,0 +1,34 @@
+//
+//  flappyPomRasTests.swift
+//  flappyPomRasTests
+//
+//  Created by Alyson Abril on 9/13/19.
+//  Copyright © 2019 Alyson Abril. All rights reserved.
+//
+
+import XCTest
+@testable import flappyPomRas
+
+class flappyPomRasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
