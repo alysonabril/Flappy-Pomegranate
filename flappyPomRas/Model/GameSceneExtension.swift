f81ca21f24cc594ad89095056e0b338caf4adc84
Jack <jackwong.professional@gmail.com>
2019-09-16 18:02:06 -0400

    Changed Background

--- flappyPomRas/Model/GameSceneExtension.swift
+++ flappyPomRas/Model/GameSceneExtension.swift
@@ -11,14 +11,35 @@ import SpriteKit
 
 extension GameScene {
     
-    func createAvatar() -> SKSpriteNode {
+    func createFruit() -> SKSpriteNode {
 
-        let avatar = SKSpriteNode(texture: SKTextureAtlas(named: "PomRas").textureNamed("pomWings"))
+        let fruit = SKSpriteNode(texture: SKTextureAtlas(named: "PomRas").textureNamed("pomWings"))
 
+        //Fruit
+        fruit.size = CGSize(width: 100, height: 100)
+        
+        fruit.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        
+        //Fruit Physics Body
+        fruit.physicsBody = SKPhysicsBody(circleOfRadius: fruit.size.width / 2)
         
         
+        //Linear Damping: This property is used to simulate fluid or air friction forces on the body. The property must be a value between 0.0 and 1.0. The default value is 0.1. If the value is 0.0, no linear damping is applied to the object.
+        fruit.physicsBody?.linearDamping = 1.0
         
-        return avatar
+        //This is how we determine the bounciness of said fruit
+        fruit.physicsBody?.restitution = 0
+        
+        fruit.physicsBody?.categoryBitMask = CollisionBitMask.avatarCategory
+        fruit.physicsBody?.collisionBitMask = CollisionBitMask.pillarCategory | CollisionBitMask.groundCategory
+        
+        fruit.physicsBody?.contactTestBitMask = CollisionBitMask.pillarCategory | CollisionBitMask.staffCategory | CollisionBitMask.groundCategory
+     
+        //Fruit is floating
+        fruit.physicsBody?.affectedByGravity = false
+        fruit.physicsBody?.isDynamic = true
+        
+        return fruit
     }
     
 }
